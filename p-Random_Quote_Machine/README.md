8f6dd80b63dea269ef479f330f28edb225f127aa
Greg Bulmash <burgerguy@gmail.com>
2021-12-11 14:39:56 -0800

    Project 1 done

--- p-Random_Quote_Machine/README.md
+++ p-Random_Quote_Machine/README.md
@@ -42,11 +42,30 @@ There are 12 tests in three groups.
 - Second (Content) is user stories (US) 1-10. PASSED 6 & 7 (why?)
 - Third (Layout) is user story 11. FAILED
 
-### 2: Pass Content test 1, Layout test 1
+### 2: Pass US 1, US 11
 
 Good project because it sets up most of the scaffolding to support the others.
 
-### 3: Pass Content tests 2 & 3
+### 3: Pass US 2 & 3 (and also 6 & 7)
 
-Interestingly enough, not only creating the elements, but filling them with a dummy (static) quote technically satisfied 6 & 7 too (except for randomness).
+Interestingly enough, not only creating the elements, but filling them with a dummy (static) quote technically satisfied 6 & 7 too, though the random quote selector (and a selection of quotes) still needs to be implemented.
 
+### 4: Pass US 4 & 5
+
+These are set up just as structure. Functionality comes with 8, 9, and 10. Created a row component, then the next button and tweet link components are child components of it. I'm pretty sure I have some .
+
+### 5: Randomize Quote Selection
+
+This adds a list of quotes, the function for selecting a random one, and a bit of logic for selecting a random quote on load. Luckily I keep some quote lists in my files. Grabbed a few, JSONned them up, and put them in the `quotes` constant.
+
+### 6: Wire up randomizer to button (US 8 & 9) 
+
+At this point 11 of 12 tests are passed.  All that's left is to wire up the tweet link.
+
+### 7: Wire up the Tweet Button
+
+This came with a bunch of misfires getting the link to render right and then for Twitter to accept it. If my target was other than "_top", Twitter would refuse the connection. 
+
+Then, when I tried to use their widget (add a JS script to the page and a class to the link to render a "tweet" button) to make it look nicer, the button would clobber my target, open a new tab, and the link would get the same refusal to connect.
+
+As that's not a requirement for the tests to pass, I decided to leave making a fancy button out of the project.
--- p-Random_Quote_Machine/index.js
+++ p-Random_Quote_Machine/index.js
@@ -1,14 +1,161 @@
-$(document).ready(function () {
-	console.log('ready!');
-});
+/* CodePen JS settings
+    JS Preprocessor: Babel
+    Libraries:
+      https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js
+      https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js
+      https://cdnjs.cloudflare.com/ajax/libs/react/17.0.2/umd/react.production.min.js
+      https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js      
+*/
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return <h1>You Gorgeous Kid</h1>;
-	}
+const quotes = [
+  {
+    quote:
+      "The problem with online quotes is you can never be sure the person to whom they're attributed actually said them.",
+    author: "Abraham Lincoln"
+  },
+  {
+    quote:
+      "It is sobering to consider that when Mozart was my age, he had already been dead for a year.",
+    author: "Tom Lehrer"
+  },
+  {
+    quote: "Never trust a man with a small black moustache.",
+    author: "P.G. Wodehouse"
+  },
+  {
+    quote: "He knew the precise psychological moment when to say nothing.",
+    author: "Oscar Wilde"
+  },
+  { quote: "I can't be overdrawn. I still have checks.", author: "Anon." },
+  {
+    quote:
+      "I don't want any yes-men around me. I want everybody to tell me the truth even if it costs them their jobs.",
+    author: "Samuel Goldwyn"
+  },
+  {
+    quote:
+      "Anyone who lives within his means suffers from a lack of imagination.",
+    author: "Lionel Stander"
+  },
+  {
+    quote:
+      "An appeaser is one who feeds a crocodile hoping it will eat him last.",
+    author: "Winston Churchill"
+  },
+  {
+    quote:
+      "When in doubt, make a fool of yourself. There is a microscopically thin line between being brilliantly creative and acting like the most gigantic idiot on earth. So what the hell, leap.",
+    author: "Cynthia Heimel"
+  },
+  {
+    quote: "There cannot be a crisis next week. My schedule is already full.",
+    author: "Henry Kissinger"
+  },
+  {
+    quote: "No one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    quote: "If we lose this war, I'll start another in my wife's name.",
+    author: "Moshe Dyan, during the Six-day war"
+  },
+  {
+    quote:
+      "I don't want a lawyer to tell me what I cannot do; I hire him to tell me how to do what I want to do.",
+    author: "J.P. Morgan"
+  },
+  {
+    quote:
+      "Let no one suppose that the words doctor and patient can disguise from the parties that they are employer and employee.",
+    author: "G.B. Shaw"
+  },
+  {
+    quote: "People become who they are. Even Beethoven became Beethoven.",
+    author: "Randy Newman"
+  },
+  {
+    quote: "Some people pay a compliment as if they expected a receipt.",
+    author: "Kin Hubbard"
+  }
+];
+
+class QuoteBox extends React.Component {
+  /* this is our "app" */
+  constructor(props) {
+    super(props);
+    let num = Math.floor(Math.random() * quotes.length);
+    this.state = {
+      currentQuote: num
+    };
+    this.nextQuote = this.nextQuote.bind(this);
+  }
+
+  nextQuote() {
+    this.setState({ currentQuote: Math.floor(Math.random() * quotes.length) });
+  }
+
+  render() {
+    return (
+      <div>
+        <b>Quote Text:</b>
+        <QuoteText quote={quotes[this.state.currentQuote].quote} />
+        <QuoteAuthor author={quotes[this.state.currentQuote].author} />
+        <LinkRow
+          tweetText={encodeURI(
+            quotes[this.state.currentQuote].quote +
+              "\n\n  -- " +
+              quotes[this.state.currentQuote].author
+          )}
+          next={this.nextQuote}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDom.render(e(App), '#root');
+const QuoteText = (props) => {
+  return <div id="text">"{props.quote}"</div>;
+};
+
+const QuoteAuthor = (props) => {
+  return <div id="author"> -- {props.author}</div>;
+};
+
+const QuoteTweet = (props) => {
+  return (
+    <div class="col-sm">
+      <a
+        href={
+          "https://twitter.com/intent/tweet?text=" +
+          props.tweetText +
+          "&related=LetMyPeopleCode"
+        }
+        id="tweet-quote"
+        target="_top"
+      >
+        Tweet
+      </a>
+    </div>
+  );
+};
+
+const NextButton = (props) => {
+  return (
+    <div class="col-sm">
+      <button onClick={props.getNext} id="new-quote">
+        Next
+      </button>
+    </div>
+  );
+};
+
+const LinkRow = (props) => {
+  return (
+    <div id="link-row" class="row" align="center">
+      <QuoteTweet tweetText={props.tweetText} />
+      <NextButton getNext={props.next} />
+    </div>
+  );
+};
+
+ReactDOM.render(<QuoteBox />, document.getElementById("quote-box"));
